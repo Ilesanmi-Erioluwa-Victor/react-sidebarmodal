afc821676d0daa74bf6aa99356cbb03d54b171ba
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-04 18:14:02 +0100

    Working on Haamburger

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,5 +3,14 @@ import { social, links } from "./Data";
 import { FaTimes } from "react-icons/fa";
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <h4 className="logo">Logo</h4>
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+    </aside>
+  );
 };
